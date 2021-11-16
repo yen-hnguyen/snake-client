305fe021f930e46f6a03290ab75c2dd96c972117
Yen <yen.hnguyen17@gmail.com>
2021-11-16 03:12:18 +0000

    Added Constant Modulee

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require('./constants');
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,
+    host: IP,
+    port: PORT,
   });
 
   conn.on('connect', () => {
---
+++ constants.js
@@ -0,0 +1,8 @@
+const IP = 'localhost';
+const PORT = 50541;
+
+
+module.exports = {
+  IP,
+  PORT,
+};
