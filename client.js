9d5a736dd794efeff1591cf6df25da9e079d3e91
Yen <yen.hnguyen17@gmail.com>
2021-11-16 00:54:54 +0000

    Refactor the code

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('data', () => {
+    console.log('you ded cuz you idled');
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
